a6647fe153d5b188b9fa98db93ab3734b21d2e0f
Oleg Marchenko <ynwa.om@gmail.com>
2018-12-01 21:09:44 +0200

    add functional module windows

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ window.addEventListener("DOMContentLoaded", function () {
       'total': t,
       'hours': hours,
       'minutes': minutes,
-      'seconds': seconds,
+      'seconds': seconds
     };
 
   }
@@ -82,4 +82,23 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   }
   setClock('timer', deadline);
+
+  //Module
+console.log(tabContent);
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        popupClose = document.querySelector('.popup-close');
+        
+
+     more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden'
+        console.log(this);
+     });
+     popupClose.addEventListener('click', function() {
+      overlay.style.display = 'none';
+      more.classList.remove('more-splash');
+      document.body.style.overflow = '';
+     });
 });
